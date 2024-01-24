d4fab828f5f1b1e7dd198aa8453f34ad9655230d
a17736 <a17736@d1-08-09.dped.local>
2024-01-24 10:55:29 +0000

    Ficha de trabalho 2

---
+++ src/main/java/turma11o/m17b_fichatrabalho2_projetodado/M17B_fichaTrabalho2_projetoDado.java
@@ -0,0 +1,31 @@
+// --- PSI M17B - projeto para a ficha de trabalho #2
+// O que é necessário fazer:
+// - criar um repositório no GitHub – sem README e tem de ser público!
+// - dar os passos necessários para fazer o Push do projeto no NetBeans para o 
+//   repositório criado anteriormente no GitHub.
+// - convidar o utilizador ProfJoaoBarbosa e outro colega à vossa escolha para
+//   serem colaboradores do repositório; todos têm de aceitar os convites.
+// - publicar o link para o repositório na folha de cálculo (Google Sheets).
+
+package turma11o.m17b_fichatrabalho2_projetodado;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Joao Barbosa
+ */
+public class M17B_fichaTrabalho2_projetoDado {
+
+    public static void main(String[] args) {
+        // Nota: este projeto tem uma outra classe, Janela, que 
+        // é executável.
+        // O código nesta classe limita-se a mostrar uma mensagem.
+        
+        JOptionPane.showMessageDialog(null,
+            "Sou o projeto dado.",
+            "Ficha de Trabalho #2",
+            JOptionPane.INFORMATION_MESSAGE);
+        
+    }
+}
